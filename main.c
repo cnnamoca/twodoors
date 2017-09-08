3024716fb935ed9c3df877e7ce2bfddf71750648
Carlo <carlo.namoca@gmail.com>
2017-09-08 13:02:53 -0700

    Added main.c

---
+++ main.c
@@ -0,0 +1,51 @@
+//
+//  main.c
+//  Assignment 2
+//
+//  Created by Carlo Namoca on 2017-08-08.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    char play;
+    int choice;
+   
+    printf("Welcome to Two doors.\n");
+    printf("Would you like to play? (y/n):");
+    scanf("%c", &play);
+    
+    while (play == 'y') {
+        
+        printf("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
+        printf("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
+        printf("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
+        printf("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
+        printf("What do you ask so you can pick the door to freedom? \n\n");
+        printf("\t 1.Ask the truth-guard to point to the door of doom. \n");
+        printf("\t 2.Ask the liar-guard to point to the door of doom. \n");
+        printf("\t 3.Doesn't matter which one you pick. \n");
+        
+        printf("\nPick an answer between choices 1-3:");
+        scanf("%d", &choice);
+        
+        char *answer = "No matter which one you choose the guards both tell you which door leads to death, and therefore you can pick the other door. \n";
+        
+        switch(choice) {
+            case 1: printf("%s \n", answer); break;
+            case 2: printf("%s \n", answer); break;
+            case 3: printf("It doesn't matter... \n"); break;
+            
+            default:
+            break;
+        }
+        printf("Play Again? y/n:");
+        scanf(" %c", &play);
+    }
+    
+    printf("Goodbye!\n");
+    
+    return 0;
+}
